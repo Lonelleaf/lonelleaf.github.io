12d2ab053c556e1458d0a013fa812c165f8ef1ea
RavelloH <hyh20060327@qq.com>
2023-04-02 14:52:56 +0800

    初始重建

---
+++ README.md
@@ -0,0 +1,5 @@
+# Loneleaf的博客  
+
+(https://loneleaf.github.io)  
+
+Powered by RavelloH's Theme (https://github.com/ravelloh/RTheme)  
---
+++ js/menu.js
@@ -0,0 +1,7 @@
+document.writeln("<li style=\"--i: 0\"><a href=\'/\'><span class=\'i_small ri:home-3-fill\'></span> 主页</a></li>");
+document.writeln("<li style=\"--i: 1\"><a href=\'/works/\'><span class=\'i_small ri:dashboard-fill\'></span> 作品</a></li>");
+document.writeln("<li style=\"--i: 2\"><a href=\'/articles\'><span class=\'i_small ri:article-fill\'></span> 文章</a></li>");
+document.writeln("<li style=\"--i: 3\"><a href=\'/tag/\'><span class=\'i_small ri:price-tag-3-fill\'></span> 标签</a></li>");
+document.writeln("<li style=\"--i: 4\"><a href=\'/about/\'><span class=\'i_small ri:information-fill\'></span> 关于</a></li><br>");
+document.writeln("<b><br><br>Copyright © <b id=\'year\'></b> Loneleaf.<br></b>");
+document.writeln("<b><br>网页设计&制作：<a href=\'https://ravelloh.gitee.io\'>RavelloH & RTheme</a></b>")
